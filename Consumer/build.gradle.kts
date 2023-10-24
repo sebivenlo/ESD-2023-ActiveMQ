8f54cf23b0a3903925d775ac7dfad7fe0c5b5a31
Nils Winking <nils@winking-online.de>
2023-10-24 14:14:35 +0200

    Add Consumer Example

---
+++ Consumer/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.activemq:artemis-core-client-all:2.31.0")
+    implementation("org.apache.activemq:artemis-server:2.31.0")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ Consumer/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.apache.activemq.artemis.api.core.ActiveMQException;
+import org.apache.activemq.artemis.api.core.QueueConfiguration;
+import org.apache.activemq.artemis.api.core.RoutingType;
+import org.apache.activemq.artemis.api.core.client.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ServerLocator locator = ActiveMQClient.createServerLocator("tcp://localhost:61616");
+
+        // In this simple example, we just use one session for both producing and receiving
+
+        ClientSessionFactory factory = locator.createSessionFactory();
+        ClientSession session = factory.createSession("artemis", "artemis", true, true, true, true, 1);
+
+        // We need a queue attached to the address ...
+        try {
+            QueueConfiguration queueConfiguration = new QueueConfiguration();
+            queueConfiguration.setAddress("example");
+            queueConfiguration.setRoutingType(RoutingType.MULTICAST);
+            queueConfiguration.setName("example");
+            queueConfiguration.setDurable(true);
+            session.createQueue(queueConfiguration);
+
+        } catch (ActiveMQException e) {
+        }
+        // And a consumer attached to the queue ...
+
+        ClientConsumer consumer = session.createConsumer("example");
+
+        // We need to start the session before we can -receive- messages ...
+        session.start();
+
+        while (true) {
+            ClientMessage msgReceived = consumer.receive();
+            System.out.println("message = " + msgReceived.getBodyBuffer().readString());
+        }
+
+    }
+}
